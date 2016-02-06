9e4a005fb2cabf286a9552df4ff885fbf18055cd
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-02-06 09:25:09 -0800

    minor updates

--- templates/aspnet5/EmptyProject/pw-templateinfo.ps1
+++ templates/aspnet5/EmptyProject/pw-templateinfo.ps1
@@ -1,58 +1,33 @@
 ﻿[cmdletbinding()]
-param(
-    [hashtable]$options = @{}
-)
-
-function Get-ValueOrDefault{
-    [cmdletbinding()]
-    param(
-        [Parameter(Position=0,Mandatory=$true)]
-        [object]$value,
-
-        [Parameter(Position=1, Mandatory=$true)]
-        [ValidateNotNull()]
-        [object]$defaultValue
-    )
-    process{
-        if($value -ne $null){
-            $value
-        }
-        else{
-            $defaultValue
-        }
-    }
-}
-
-$projectName = (Get-ValueOrDefault $options['ProjectName'] 'MyWebProject')
-$guid1 = [System.Guid]::NewGuid()
-[System.IO.DirectoryInfo]$destDir = (Get-ValueOrDefault $options['DestDir'] '.\')
-$solutionDir = (Get-ValueOrDefault $options['SolutionDir'] '..\..\')
-$artifactsDir = (Get-ValueOrDefault $options['ArtifactsDir'] ($solutionDir + 'artifacts\'))
-
-
-# capturing the output directly may not be the best idea here because it's too easy to output something accidently
-# instead the user will call a method to set the result
+param()
 
 $templateInfo = New-Object -TypeName psobject -Property @{
     Name = 'aspnet5-empty'
     Description = 'ASP.NET 5 empty project'
-    ProjectName = 'MyEmptyProject'
-    SolutionDir = $solutionDir
-    ArtifactsDir = $artifactsDir
+    DefaultProjectName = 'MyEmptyProject'
+    LicenseUrl = 'https://raw.githubusercontent.com/ligershark/pecan-waffle/master/LICENSE'
+    ProjectUrl = 'https://github.com/ligershark/pecan-waffle'
+    GitUrl = 'https://github.com/ligershark/pecan-waffle.git'
+    GitBranch = 'master'
 }
 
 $templateInfo.ArtifactsDir = $templateInfo.SolutionDir
 
-# TODO hook up replacements here as well. It can call a method to register the replacement. The key will be the containing folder
+Add-Replacement $templateInfo 'EmptyProject' {$ProjectName} {$DefaultProjectName}
+Add-Replacement $templateInfo 'SolutionDir' {$solutionDir} {'..\..\'}
+Add-Replacement $templateInfo 'ArtifactsDir' {$solutionDir+'artifacts\'} {'..\..\artifacts'}
+Add-Replacement '97b148d4-829e-4de3-840b-9c6600caa117' {$ProjectId} {[System.Guid]::NewGuid()}
+Add-Replacement '97b148d4-829e-4de3-840b-9c6600caa117' {$ProjectId} {[System.Guid]::NewGuid()} -rootDir = 'sub\wwwroot' -Include '*','**' -Exclude '*.6','*.1'
 
-Add-Replacement $templateInfo 'EmptyProject' {$ProjectName}
-Add-Replacement '97b148d4-829e-4de3-840b-9c6600caa117' {[System.Guid]::NewGuid()}
-Add-Replacement '97b148d4-829e-4de3-840b-9c6600caa117' [System.Guid]::NewGuid()} -rootDir = 'sub\wwwroot' -Include '*','**' -Exclude '*.6','*.1'
+# when the template is run any filename with the given string will be updated
+Update-FileNames $templateInfo 'EmptyProject' {$EmptyProject}
+# excludes files from the template
+Exclude-Files $templateInfo '*.user','*.suo','*.userosscache','project.lock.json','*.vs*scc'
+# excludes folders from the template
+Exclude-Folders $templateInfo '.vs','artifacts'
 
-# TODO: Implement this function
-# This will basically store the templateinfo object so that the caller can reterieve it
-# or it could set the template info into a global object somewhere that's indexed by the fullpath
-# of folder?
+
+# This will register the template with pecan-waffle
 Set-TemplateInfo -templateInfo $templateInfo
 
 
