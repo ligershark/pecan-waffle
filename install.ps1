708551cf3d609fec4db2e44022a92eefb98ea601
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-02-07 09:36:38 -0800

    updating install script

--- install.ps1
+++ install.ps1
@@ -1,14 +1,69 @@
-﻿$url = 'https://raw.githubusercontent.com/ligershark/pecan-waffle/master/pecan-waffle.psm1'
-[System.IO.FileInfo]$destfile = ('{0}\pecan-waffle\install\pecan-waffle.psm1' -f $env:LOCALAPPDATA)
-
-if(-not (Test-Path $destfile.Directory.FullName)){
-    New-Item -Path $destfile.Directory.FullName -ItemType Directory
+﻿function EnsureDirectoryExists{
+    param([Parameter(Position=0)][System.IO.DirectoryInfo]$path)
+    process{
+        if($path -ne $null){
+            if(-not (Test-Path $path.FullName)){
+                New-Item -Path $path.FullName -ItemType Directory
+            }
+        }
+    }
 }
-if(Test-Path $destfile.FullName){
-    Remove-Item $destfile.FullName
+
+function GetPsModulesPath{
+    [cmdletbinding()]
+    param()
+    process{
+        $Destination = $null
+        if(Test-Path 'Env:PSModulePath'){
+            $ModulePaths = @($Env:PSModulePath -split ';')
+
+            $ExpectedUserModulePath = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+            $Destination = $ModulePaths | Where-Object { $_ -eq $ExpectedUserModulePath}
+            if (-not $Destination) {
+                $Destination = $ModulePaths | Select-Object -Index 0
+            }
+        }
+        $Destination
+    }
 }
-(New-Object System.Net.WebClient).DownloadFile($url, $destfile.FullName)
 
-Import-Module $destfile.FullName -Global
+# 1. download .zip file from github
+[System.IO.DirectoryInfo]$tempinstalldir = ('{0}\pecan-waffle\install\' -f $env:LOCALAPPDATA)
+$zipurl = 'https://github.com/ligershark/pecan-waffle/archive/master.zip'
+[System.IO.FileInfo]$tempzipdest = (join-path $tempinstalldir.FullName 'pecan-waffle.zip')
+EnsureDirectoryExists $tempzipdest.Directory.FullName
 
-Add-TemplateSource -url 'https://github.com/ligershark/pecan-waffle.git'
+if(Test-Path $tempzipdest.FullName){
+    Remove-Item $tempzipdest.FullName
+}
+(New-Object System.Net.WebClient).DownloadFile($zipurl,$tempzipdest.FullName)
+
+# 2. extract .zip file to a temp dir in %localappdata%
+[System.IO.DirectoryInfo]$tempextractdir = (join-path ($tempinstalldir.FullName) ([System.Guid]::NewGuid()))
+EnsureDirectoryExists $tempextractdir.FullName
+
+Add-Type -assembly “system.io.compression.filesystem”
+[io.compression.zipfile]::ExtractToDirectory($tempzipdest.FullName, $tempextractdir.FullName)
+
+$foldertocopy = ([System.IO.DirectoryInfo](Join-Path $tempextractdir.FullName 'pecan-waffle-master')).FullName
+
+# 4. copy contents to ps modules folder
+[System.IO.DirectoryInfo]$moduledestfolder = (Join-Path (GetPsModulesPath) 'pecan-waffle')
+EnsureDirectoryExists $moduledestfolder.FullName
+Copy-Item $foldertocopy\* -Destination $moduledestfolder.FullName -Include * -Recurse
+
+# 5. load module
+[System.IO.FileInfo]$expectedmodpath = (Join-Path $moduledestfolder.FullName 'pecan-waffle.psm1')
+if(-not (Test-Path $expectedmodpath.FullName)){
+    throw ('Did not find module at [{0}]' -f $expectedmodpath.FullName)
+}
+
+Remove-Module pecan-waffle -ErrorAction SilentlyContinue
+Import-Module $expectedmodpath.FullName -Global
+
+# 6. add template source
+Add-TemplateSource -path $expectedmodpath.Directory.FullName
+
+if(Test-Path $tempextractdir.FullName){
+    Remove-Item $tempextractdir.FullName -Recurse -ErrorAction SilentlyContinue
+}
