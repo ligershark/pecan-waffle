87c8422c580a5eeebd74a1c1ca64cd72e1651b95
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-02-07 17:34:44 -0800

    updating appveyor.ps1

--- appveyor.ps1
+++ appveyor.ps1
@@ -9,7 +9,7 @@ Remove-Module pecan-waffle -Force -ErrorAction SilentlyContinue
 Import-Module $modPath.FullName -DisableNameChecking -Global
 
 if(Test-Path '.\.appveyor'){
-    Remove-Item '.\.appveyor' -Recurse
+    Remove-Item '.\.appveyor' -Recurse -Force
 }
 
 [System.IO.DirectoryInfo]$destDir = '.\.appveyor\dest'
@@ -17,8 +17,13 @@ if(-not (Test-Path $destDir.FullName)){
     New-Item -Path $destDir.FullName -ItemType Directory
 }
 
+'project: demo-singlefileproj' | Write-Host
 Add-Project -templateName 'demo-singlefileproj' -destPath (join-path $destDir.FullName 'single') -projectName 'DemoProjSingleItem'
+'project: empty' | Write-Host
 Add-Project -templateName 'aspnet5-empty' -destPath (join-path $destDir.FullName 'empty') -projectName 'MyNewEmptyProj'
+'project: api' | Write-Host
 Add-Project -templateName 'aspnet5-webapi' -destPath (join-path $destDir.FullName 'api') -projectName 'MyNewApiProj'
+'Item: controllerjs ' | Write-Host
 Add-Item -templateName 'demo-controllerjs' -destPath (join-path $destDir.FullName 'item-controllerjs') -itemName 'newcontroller'
+'Item: angular' | Write-Host
 Add-Item -templateName 'demo-angularfiles' -destPath (join-path $destDir.FullName 'item-demo-angularfiles') -itemName 'newcontroller'
