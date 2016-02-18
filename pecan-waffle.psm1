2f0fd8e785174ba883d32f3f4abdd9a3f67c4f5e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-02-18 09:41:11 -0800

    adding aspnet item templates

--- pecan-waffle.psm1
+++ pecan-waffle.psm1
@@ -994,5 +994,5 @@ if( ($env:IsDeveloperMachine -eq $true) ){
     Export-ModuleMember -function * -Alias *
 }
 else{
-    Export-ModuleMember -function Get-*,Set-*,Invoke-*,Save-*,Test-*,Find-*,Add-*,Remove-*,Test-*,Open-*,New-*,Import-* -Alias *
+    Export-ModuleMember -function Get-*,Set-*,Invoke-*,Save-*,Test-*,Find-*,Add-*,Remove-*,Test-*,Open-*,New-*,Import-*,Clear-* -Alias *
 }
