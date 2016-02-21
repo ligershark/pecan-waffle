df99cfb7f10cc31a56d69f0cb01a430717aec6c9
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-02-20 16:52:11 -0800

    adding vs projects

---
+++ vs-src/PecanWaffle/PecanWaffleWizard.cs
@@ -0,0 +1,15 @@
+﻿namespace PecanWaffle {
+    using EnvDTE;
+    using EnvDTE80;
+    using Microsoft.VisualStudio.TemplateWizard;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PecanWaffleWizard {
+    }
+}
