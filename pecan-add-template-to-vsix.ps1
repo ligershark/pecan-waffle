b7ac22cfc30f0d7990c5756129966d1cb0f41f90
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-04-22 17:48:16 -0700

    updating version to 0.0.14-beta

--- pecan-add-template-to-vsix.ps1
+++ pecan-add-template-to-vsix.ps1
@@ -28,7 +28,7 @@ $env:EnableAddLocalSourceOnLoad =$false
 # parameters declared here
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted | out-null
 
-[System.Version]$minPwVersion = (New-Object -TypeName 'system.version' -ArgumentList '0.0.13.0')
+[System.Version]$minPwVersion = (New-Object -TypeName 'system.version' -ArgumentList '0.0.14.0')
 $pwNeedsInstall = $true
 
 # see if pw is already installed and has a high enough version
