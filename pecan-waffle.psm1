bc04913f96fce77a364fe1882e0d4a8e454b81d4
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-05-11 17:28:28 -0700

    updating verstion to 0.0.19-beta

--- pecan-waffle.psm1
+++ pecan-waffle.psm1
@@ -61,7 +61,7 @@ function InternalGet-ScriptDirectory{
 function Get-PecanWaffleVersion{
     param()
     process{
-        New-Object -TypeName 'system.version' -ArgumentList '0.0.18.0'
+        New-Object -TypeName 'system.version' -ArgumentList '0.0.19.0'
     }
 }
 
